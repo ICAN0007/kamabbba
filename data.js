309bb52f5bada35ebab9ddb2412c651561b1615a
ICAN0007 <icankissyou0007@gmail.com>
2025-07-28 22:26:29 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,102 @@
+const mediaItems = [
+  {
+    id: "v1",
+    title: "nude",
+    categories: ["viral"],
+    tags: ["funny"],
+    src: "https://files.catbox.moe/l2524k.mp4",
+    thumb: "https://files.catbox.moe/77z1vd.png",
+    duration: 200,
+    addedAt: "2025-07-19T09:00:00Z"
+  },
+  {
+    id: "v2",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/6qzodz.mp4",
+    thumb: "https://files.catbox.moe/ubwr6d.png",
+    duration: 250,
+    addedAt: "2025-07-18T09:00:00Z"
+  },
+  {
+    id: "v3",
+    title: "Desi VIP onlyfans indian the star of us i will sucks your dick well but fuck me better.",
+    categories: ["Indian"],
+    tags: ["Porn", "Sex", "Dick", "Cock", "Pussy", "Tits", "Ass", "Kissing", "Suck", "Blowjob", "spreading", "licking", "Missionary", "doggystyle", "cowgirl"],
+    src: "https://files.catbox.moe/iidf41.mp4",
+    thumb: "https://files.catbox.moe/7xjtwb.png",
+    duration: 1200,
+    addedAt: "2025-07-25T00:18:00Z"
+  },
+  {
+    id: "v4",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/ydxh68.mp4",
+    thumb: "https://files.catbox.moe/x13o1w.png",
+    duration: 250,
+    addedAt: "2025-07-19T09:00:00Z"
+  },
+  {
+    id: "v5",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/7dt5ce.mp4",
+    thumb: "https://files.catbox.moe/4v4vcn.png",
+    duration: 250,
+    addedAt: "2025-07-19T09:00:00Z"
+  },
+  {
+    id: "v6",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/iipfhl.mp4",
+    thumb: "https://files.catbox.moe/rhrtox.png",
+    duration: 250,
+    addedAt: "2025-07-19T09:00:00Z"
+  },
+  {
+    id: "v7",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/d583aq.mp4",
+    thumb: "https://files.catbox.moe/5a0wa0.png",
+    duration: 250,
+    addedAt: "2025-07-19T09:00:00Z"
+  },
+  {
+    id: "v8",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/v80cm0.mp4",
+    thumb: "https://files.catbox.moe/jr0udr.png",
+    duration: 250,
+    addedAt: "2025-07-19T09:00:00Z"
+  },
+  {
+    id: "v9",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/o4aqqn.mp4",
+    thumb: "https://files.catbox.moe/7pde9k.png",
+    duration: 250,
+    addedAt: "2025-07-19T09:00:00Z"
+  },
+  {
+    id: "v10",
+    title: "sexy indian girl goes nude",
+    categories: ["latest"],
+    tags: ["trending"],
+    src: "https://files.catbox.moe/ydxh68.mp4",
+    thumb: "https://files.catbox.moe/x13o1w.png",
+    duration: 250,
+    addedAt: "2025-07-19T09:00:00Z"
+  }
+];
